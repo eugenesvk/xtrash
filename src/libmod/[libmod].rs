20c51dbf0d7684089e377de6d94ac352693aee54
eugenesvk <eugenesvk@users.noreply.github.com>
2024-11-29 17:54:35 +0700

    add _unsudo function

--- src/libmod/[libmod].rs
+++ src/libmod/[libmod].rs
@@ -1,1 +1,7 @@
 pub fn ret42() -> i32 { 42 }
+/// Set process ID to that of the logged in user (to not trash to root trash, but user's trash, but then you might have no permissions)
+pub fn _unsudo() {
+  if let   Ok(regu_uid) = get_logged_uid() {//trace!("setting regular {}",&regu_uid);
+    unsafe { libc::setuid(regu_uid); }
+  }
+}
