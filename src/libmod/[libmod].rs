98777215d5d2dfe2d37d89620f3c982ccedcf663
eugenesvk <eugenesvk@users.noreply.github.com>
2024-11-30 23:28:14 +0700

    add a stub for lossy-percent encoding of invalid utf8 bytes

--- src/libmod/[libmod].rs
+++ src/libmod/[libmod].rs
@@ -43,3 +43,26 @@ pub fn _unsudo() {
     unsafe { libc::setuid(regu_uid); }
   }
 }
+
+use std::borrow::Cow;
+pub fn from_utf8_lossy_pc(v:&[u8]) -> Cow<'_, str> { // std's from_utf8_lossy, but instead of invalid replacement char %-encode the byte
+  let mut iter = v.utf8_chunks();
+
+  let first_valid = if let Some(chunk) = iter.next() {
+    let valid = chunk.valid();
+    if chunk.invalid().is_empty() {debug_assert_eq!(valid.len(), v.len()); // invalid=empty → last chunk
+      return     Cow::Borrowed(valid);}
+    valid
+  } else {return Cow::Borrowed(""   );};
+  const REPLACEMENT: &str = "\u{FFFD}";
+
+  let mut res = String::with_capacity(v.len());
+  res.push_str(first_valid);
+  res.push_str(REPLACEMENT);
+
+  for chunk in iter                {res.push_str(chunk.valid());
+    if !chunk.invalid().is_empty() {res.push_str(REPLACEMENT);}
+  }
+  Cow::Owned(res)
+}
+
